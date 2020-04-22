0905b448f6a1824688a89dc45e465f6dc9916b84
Harsha.Pulluru <Harsha.Pulluru@blujaysolutions.com>
2020-04-23 00:29:18 +0530

    Updated Merge Sort

--- src/main/java/harsha/practice/sorting/MergeSort.java
+++ src/main/java/harsha/practice/sorting/MergeSort.java
@@ -1,6 +1,46 @@
 package harsha.practice.sorting;
 
-/* Java program for Merge Sort */
 public class MergeSort {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 5, 3, 1, 8, 9 };
+		mergeSort(arr);
+	}
+
+	private static void mergeSort(int[] fullArr) {
+		if(fullArr.length==1) {
+			return;
+		}
+		int midIx = (fullArr.length / 2) + (fullArr.length % 2);
+		int[] firstHalfArr = new int[midIx];
+		int[] secondHalfArr = new int[fullArr.length - midIx];
+		split(fullArr, firstHalfArr, secondHalfArr);
+		mergeSort(firstHalfArr);
+		mergeSort(secondHalfArr);
+		/*System.out.println("yo");
+		print(firstHalfArr);
+		System.out.println("yo1");*/
+		//print(fullArr);
+	}
+
+	private static void print(int[] arr) {
+		for (int x : arr) {
+			System.out.println(x);
+		}
+		System.out.println("yo1");
+	}
+
+	private static void split(int[] fullArr, int[] firstHalfArr, int[] secondHalfArr) {
+		int secondArrStrtIndex = firstHalfArr.length;
+		for (int i = 0; i < firstHalfArr.length + secondHalfArr.length; i++) {
+			if (i < secondArrStrtIndex) {
+				firstHalfArr[i] = fullArr[i];
+			} else {
+				secondHalfArr[i - secondArrStrtIndex] = fullArr[i];
+			}
+		}
+		/*print(firstHalfArr);
+		print(secondHalfArr);*/
+	}
 }
-/* This code is contributed by Rajat Mishra */
--- src/main/java/harsha/pratice/recursions/FactorialRecursion.java
+++ src/main/java/harsha/pratice/recursions/FactorialRecursion.java
@@ -4,7 +4,16 @@ public class FactorialRecursion {
 
 	public static void main(String[] args) {
 
-		System.out.println(fact(4));
+		fact(4);
+		printNumber(4);
+	}
+
+	private static void printNumber(int i) {
+		System.out.println("value is " + i);
+		if (i == 1) {
+			return;
+		}
+		printNumber(i - 1);
 	}
 
 	static int fact(int n) {
@@ -12,7 +21,7 @@ public class FactorialRecursion {
 			return 1;
 		} else {
 			System.out.println(n);
-			int a= n * fact(n - 1);
+			int a = n * fact(n - 1);
 			System.out.println(n);
 			return a;
 		}
